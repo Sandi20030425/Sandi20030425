6afc0263a0339aa61d911390f28506fd3df32bbc
Sandi20030425 <botalltier5v@gmail.com>
2025-01-02 20:18:38 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+TOKEN = os.getenv("7802166659:AAFTzQphGhKhimOHx_KoTT0NkskjwOedBck")
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Halo, saya adalah bot Anda!")
+
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(f"Kamu berkata: {update.message.text}")
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
